efd21e50181541a5c0d7ab9bb3ac6b78152eb22a
Renato Henrique <renato.henrique_98@hotmail.com>
2020-09-04 20:09:21 -0300

    Add cors on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import "express-async-errors";
 
 import routes from "./routes";
 
+import cors from "cors";
+
 import { started } from "./utils/logger";
 import { DIRECTORY } from "./config/uploading";
 
@@ -15,6 +17,7 @@ import "./database";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use("/files", express.static(DIRECTORY));
 app.use(routes);
