2679bad10540a9faebdcd95ca52a48ce476b96e5
Renato Henrique <renato.henrique_98@hotmail.com>
2020-09-19 16:29:52 -0300

    Começando app mobile do modulo 3

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import { Container } from "./styles";
+
+const SignIn: FC = () => {
+
+    return (
+        <Container />
+    )
+};
+
+export default SignIn;
---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import { Container } from "./styles";
+
+const SignUp: FC = () => {
+
+    return (
+        <Container />
+    )
+};
+
+export default SignUp;
