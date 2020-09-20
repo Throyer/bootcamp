5e2e29dea9121d39d1adbed843186a0fb2af311b
Renato Henrique <renato.henrique_98@hotmail.com>
2020-09-19 22:19:23 -0300

    Mais mudanças no mobile

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,12 +1,36 @@
 import React, { FC } from 'react';
+import { Image } from 'react-native';
 
-import { Container } from "./styles";
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { LOGO } from '../../utils/assets';
+
+import { Container, Title } from "./styles";
 
 const SignIn: FC = () => {
 
     return (
-        <Container />
-    )
+        <Container>
+            <Image source={LOGO} />
+
+            <Title>Faça seu logon</Title>
+
+            <Input
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+            />
+            
+            <Input
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+            />
+            
+            <Button onPress={() => console.log("pressionou")}>Entrar</Button>
+        </Container>
+    );
 };
 
 export default SignIn;
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components/native";
 
+import { ROBOTO_SLAB_MEDIUM } from "../../utils/fonts";
+
 export const Container = styled.View`
-    
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    color: #f4ede8;
+    font-family: '${ROBOTO_SLAB_MEDIUM}';
+    margin: 64px 0 24px;
 `;
